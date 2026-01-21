0c2b0ec90c931dfd64ca7d5a30d5697934d211df
dmontaner <david.montaner@gmail.com>
2026-01-21 12:32:59 +0000

    handle server stop

--- e010_server.py
+++ e010_server.py
@@ -3,12 +3,18 @@ import socketserver
 import os
 
 PORT = 8001
-DIRECTORY = "/home/dmontaner/code/2025/dominik_2/docs"  # folder to serve
+DIRECTORY = os.path.join(os.path.dirname(__file__), "docs")  # folder to serve
 
 os.chdir(DIRECTORY)
 
 Handler = http.server.SimpleHTTPRequestHandler
 
+# Allow reuse of the address to prevent "Address already in use" errors
+socketserver.TCPServer.allow_reuse_address = True
+
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving {DIRECTORY} at http://localhost:{PORT}")
-    httpd.serve_forever()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped.")
